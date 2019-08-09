b5a890172f57f4d1548da5dcfec643e92735d007
zubeir-Abubakar <zubkayare@gmail.com>
2019-08-09 10:40:42 +0300

    solving an error where i forgot to add (+) in the hoods url in the neighbourhood urls

--- hood/urls.py
+++ hood/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
   url(r'^$',views.home,name='home'),
   url(r'^accounts/',include('registration.backends.simple.urls')),
-  url(r'^hood/(?P<neighborhood_id>[0-9])$',views.hoods,name='hood-details'),
+  url(r'^hood/(?P<neighborhood_id>[0-9]+)$',views.hoods,name='hood-details'),
   url(r'^new/neighborhood$',views.new_hood,name='newHood'),
   url(r'^new/neighbour$',views.new_neighbour,name='newNeighbour'),
   url(r'^new/business$',views.new_business,name='newBusiness'),
