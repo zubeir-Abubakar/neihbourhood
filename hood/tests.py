fae05171530f15b2bc761e9ed8a8a45c9e3984fb
zubeir-Abubakar <zubkayare@gmail.com>
2019-08-13 08:34:45 +0300

    creating tests and running them

--- hood/tests.py
+++ hood/tests.py
@@ -4,46 +4,46 @@ from .models import Neighborhood,NeighborProfile,Business
 
 
 
-# class ProjectTestClass(TestCase):
+class ProjectTestClass(TestCase):
 
-#     # Set up method
-#     def setUp(self):
-#         self.Blog = Project(
-#             name='Blog', link='https://zubeyr.herokuapp.com/authenticate/register')
+    # Set up method
+    def setUp(self):
+        self.koma = neighbourhood(
+            name='koma', link='https://zubeyr.herokuapp.com/authenticate/register')
 
-#         # Testing  instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.Blog, Project))
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Blog, Project))
 
-#         # Testing Save Method
-#     def test_save_method(self):
-#         self.Blog.project_save()
-#         Projects = Project.objects.all()
-#         self.assertTrue(len(projects) > 0)
+        # Testing Save Method
+    def test_save_method(self):
+        self.koma.neighburhood_save()
+        neighbours = neighbourhood.objects.all()
+        self.assertTrue(len(projects) > 0)
 
-#     def test_save_method(self):
-#         self.Blog.project_delete()
-#         projects = Project.objects.all()
-#         self.assertTrue(len(projects) > 0)
+    def test_save_method(self):
+        self.koma.neighbourood_delete()
+        neighbours = Neighbourhood.objects.all()
+        self.assertTrue(len(neighbours) > 0)
 
 
-# class ProfileTestClass(TestCase):
+class ProfileTestClass(TestCase):
 
-#     # Set up method
-#     def setUp(self):
-#         self.0743046778 = Profile(name='Arsenal', Bio='ARSENAL', Contact='0743046778')
+    # Set up method
+    def setUp(self):
+        self.0743046778 = Profile(name='Arsenal', Bio='ARSENAL', Contact='0743046778')
 
-#         # Testing  instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.0743046778, Profile))
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.0743046778, Profile))
 
-#         # Testing Save Method
-#     def test_save_method(self):
-#         self.0743046778.profile_save()
-#         profiles = profiles.objects.all()
-#         self.assertTrue(len(profiles) > 0)
+        # Testing Save Method
+    def test_save_method(self):
+        self.0743046778.profile_save()
+        profiles = profiles.objects.all()
+        self.assertTrue(len(profiles) > 0)
 
-#     def test_save_method(self):
-#         self.0743046778.profile_delete()
-#         profiles = Profile.objects.all()
-#         self.assertTrue(len(profiles) > 0)
+    def test_save_method(self):
+        self.0743046778.profile_delete()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
